03509e03e72784c9a7d104b9ab888d220b1c7e0e
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-11 15:16:56 +0330

    add file style css our heder 1

--- src/components/header/Header1.jsx
+++ src/components/header/Header1.jsx
@@ -1,11 +1,39 @@
-import   { useContext } from "react";
+import   { useContext, useState } from "react";
 import { ColorModeContext } from "../../theme";
-import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, List, ListItem, ListItemText, Menu, MenuItem, Stack, Typography, useTheme } from "@mui/material";
 import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
+
+const options = [
+  'Ar',
+  'En'
+
+];
+
 
 const Header1 = () => {
   const colorMode = useContext(ColorModeContext);
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const open = Boolean(anchorEl);
+  const handleClickListItem = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event,index ) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
 
     <Box sx={{bgcolor:"#2B3445"}}>
@@ -29,6 +57,8 @@ const Header1 = () => {
 
 free expres shopi
 </Typography>
+
+<Box flexGrow={1} />
  {/* start darkmodd icon */}
     <div>
     {theme.palette.mode === "light" ? (
@@ -42,7 +72,7 @@ free expres shopi
         }}
         color="inherit"
       >
-        <LightModeOutlined/>
+        <LightModeOutlined fontSize="small"/>
       </IconButton>
     ) : (
       <IconButton
@@ -55,11 +85,59 @@ free expres shopi
         }}
         color="inherit"
       >
-        <DarkModeOutlined />
+        <DarkModeOutlined fontSize="small" />
       </IconButton>
     )}
   </div>
   {/* end darkmoddd */}
+
+
+
+  <List
+        component="nav"
+        aria-label="Device settings"
+        sx={{ bgcolor: 'background.paper' }}
+      >
+        <ListItem
+          button
+          id="lock-button"
+          aria-haspopup="listbox"
+          aria-controls="lock-menu"
+          aria-label="when device is locked"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClickListItem}
+        >
+          <ListItemText
+            primary="When device is locked"
+            secondary={options[selectedIndex]}
+          />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'lock-button',
+          role: 'listbox',
+        }}
+      >
+        {options.map((option, index) => (
+          <MenuItem
+            key={option}
+            disabled={index === 0}
+            selected={index === selectedIndex}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+
+  <TwitterIcon fontSize="small"  />
+  <FacebookIcon fontSize="small" />
+  <InstagramIcon  fontSize="small"/>
   </Stack>
   </Box>
   )
