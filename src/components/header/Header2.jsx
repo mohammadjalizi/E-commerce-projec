9f4eeb977c09d57980278d874c29e7a2ade9621d
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-12 14:20:13 +0330

    start code the header2

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -1,8 +1,17 @@
+import { ShoppingCartOutlined } from '@mui/icons-material'
+import { Container, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const Header2 = () => {
   return (
-    <div>Header2DDD</div>
+    <Container>
+<Stack>
+
+<ShoppingCartOutlined/>
+<Typography>   E-commerce  </Typography>
+</Stack>
+
+   </Container>
   )
 }
 
