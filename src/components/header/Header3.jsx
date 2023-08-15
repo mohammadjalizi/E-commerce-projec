c9b2cd4054655a790d051fcd17b7b82510f69fae
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-15 13:45:00 +0330

    Added accordion on header3

--- src/components/header/Header3.jsx
+++ src/components/header/Header3.jsx
@@ -7,6 +7,12 @@ import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from "@mui/icons-material/Menu";
 import WindowIcon from "@mui/icons-material/Window";
 import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import {
   SportsEsportsOutlined,
   ElectricBikeOutlined,
@@ -146,6 +152,21 @@ justifyContent:"space-between"
       <CloseIcon/>
               
     </IconButton>
+    <Accordion elevation={0} sx={{ bgcolor:"initial" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 </Box >
           </Drawer>
     </Container>
