d072c8bff7f7cf3abca3c5ac7187953473f1c9df
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-16 18:09:43 +0330

    Add images for

--- src/components/heroseaction/Hero.jsx
+++ src/components/heroseaction/Hero.jsx
@@ -1,21 +1,146 @@
-import { Box, Container } from '@mui/material'
+import { TypeSpecimenTwoTone } from '@mui/icons-material'
+
 import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Link,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Hero = () => {
   return (
-   <Container sx={{ mt:2.5,display:"flex", alignItems:"center"}}  >
+   <Container sx={{ mt:2.5,display:"flex", alignItems:"center" }}  >
 
-<Box>
+<Box flexGrow={1}>
   slider
 </Box>
     
 <Box>
 
-<Box>
-  img1
+<Box sx={{position:"relative"}}>
+ <img src='src\image\banner-17.jpg' alt=''/>
+ 
+ <Stack
+              sx={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                left: 31,
+              }}
+            >
+              <Typography
+                variant="caption"
+                sx={{
+                  color: "#2B3445",
+                  fontSize: "18px",
+                }}
+              >
+                NEW ARRIVALS
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                  lineHeight: "16px",
+                  mt: 1,
+                }}
+              >
+                SUMMER
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                }}
+              >
+                SALE 20% OFF
+              </Typography>
+
+              <Link
+                sx={{
+                  color: "#2B3445",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                  transition: "0.2s",
+
+                  "&:hover": {
+                    color: "#D23F57",
+                  },
+                }}
+                href="#"
+                underline="none"
+              >
+                shop now
+                <ArrowForwardIcon sx={{ fontSize: "13px" }} />
+              </Link>
+            </Stack>
+
 </Box>
-   <Box>
-    imge2
+<Box sx={{ position: "relative" }}>
+  <img src='src\image\banner-16.jpg' alt=''/>
+  <Stack
+              sx={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                left: 31,
+              }}
+            >
+              <Typography
+                variant="caption"
+                sx={{
+                  color: "#2B3445",
+                  fontSize: "18px",
+                  fontWeight: 300,
+                }}
+              >
+                GAMING 4K
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                  lineHeight: "16px",
+                  mt: 1,
+                }}
+              >
+                DESKTOPS &
+              </Typography>
+
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                }}
+              >
+                LAPTOPS
+              </Typography>
+
+              <Link
+                sx={{
+                  color: "#2B3445",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                  transition: "0.2s",
+
+                  "&:hover": {
+                    color: "#D23F57",
+                  },
+                }}
+                href="#"
+                underline="none"
+              >
+                shop now
+                <ArrowForwardIcon sx={{ fontSize: "13px" }} />
+              </Link>
+            </Stack>
 </Box> 
 
 </Box>
