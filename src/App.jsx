ee1515da04641c226ed6f539a019c7c7e4ee1d5d
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-24 16:53:06 +0330

    restore file

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Header1 from "./components/header/Header1";
-import Header2 from "./components/header/Header2";
+
 import Header3 from "./components/header/Header3";
 import Hero from "./components/heroseaction/Hero";
+import Header2 from "./components/header/Header2";
 
 
 function App() {
   const [theme, colorMode] = useMode();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
+    <ColorModeContext.Provider 
+// @ts-ignore
+    value={colorMode}>
+      <ThemeProvider 
+// @ts-ignore
+      theme={theme}>
         <CssBaseline />
       <Header1/>
       <Header2/>
