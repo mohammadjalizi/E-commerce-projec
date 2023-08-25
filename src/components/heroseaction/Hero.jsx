defac6d6c7b78f9e3a56775858436d6bf54cd2b8
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-25 13:26:20 +0330

    Add arry myslider

--- src/components/heroseaction/Hero.jsx
+++ src/components/heroseaction/Hero.jsx
@@ -1,12 +1,38 @@
-import { Box, Container } from '@mui/material'
+import { Navigation } from '@mui/icons-material'
+import { Box, Container} from '@mui/material'
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./slider.css";
+
+// import required modules
+
+const mySlider = [
+  { text: "MEN", link: "src\image\banner-15.jpg" },
+  { text: "WOMEN", link: ".//images/banner-25.jpg" },
+];
 
 const Hero = () => {
   return (
    <Container sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
 <Box>
 
-slider
+<Swiper
+          // loop={true}
+          pagination={{
+            dynamicBullets: true,
+          }}
+          // modules={[Pagination]}
+          className="mySwiper"
+        >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+      
+      </Swiper>
 
 
 </Box>
