c0d8148007f55c2fb5961e79e7d0afb5b2a9ba3a
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-25 14:07:46 +0330

    install swiper

--- src/components/heroseaction/Hero.jsx
+++ src/components/heroseaction/Hero.jsx
@@ -1,56 +1,25 @@
-import { Navigation } from '@mui/icons-material'
-import { Box, Container} from '@mui/material'
-import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "./slider.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './Slider.css';
 
 // import required modules
-
-const mySlider = [
-  { text: "MEN", link: "src\image\banner-15.jpg" },
-  { text: "WOMEN", link: ".//images/banner-25.jpg" },
-];
+import { Pagination } from 'swiper/modules';
 
 const Hero = () => {
   return (
-   <Container sx={{display:"flex",justifyContent:"space-between",alignItems:"center"}}>
-<Box>
-
-<Swiper
-          // loop={true}
-          pagination={{
-            dynamicBullets: true,
-          }}
-          // modules={[Pagination]}
-          className="mySwiper"
-        >
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
-      
+
       </Swiper>
-
-
-</Box>
-
-<Box>
-
-<Box>
-
-<img src='src\image\banner-16.jpg'/>
-</Box>
-<Box>
- <img src='src\image\banner-17.jpg'/>
-</Box>
-</Box>
-
-
-   </Container>
-  )
+    </>
+  );
 }
-
-export default Hero
+export default Hero;
