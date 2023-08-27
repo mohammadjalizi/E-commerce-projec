694b627d1c378aa152ae0cc23c2a00441fe666da
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-27 05:26:31 +0330

    end slider image

--- src/components/heroseaction/Hero.jsx
+++ src/components/heroseaction/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Link, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, Link, Stack, Typography, useTheme } from '@mui/material'
 import React from 'react'
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
@@ -12,10 +12,14 @@ import "./slider.css";
 import { Pagination } from 'swiper/modules';
 
 
-
+const mySlider = [
+  { text: "MEN", link: "./images/banner-15.jpg" },
+  { text: "WOMEN", link: ".//images/banner-25.jpg" },
+];
 const Hero = () => {
+  const theme = useTheme();
   return (
-  <Container sx={{mt:2.5,display:"flex",alignItems:"center"}}>
+  <Container sx={{mt:2.5,display:"flex",alignItems:"center",gap:2}}>
 
 <Swiper
         pagination={{
@@ -24,14 +28,98 @@ const Hero = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-<img src='src\image\banner-15.jpg '/>
+        {mySlider.map((item)=>{
 
-        </SwiperSlide>
-        <SwiperSlide>
-<img src=' src\image\banner-25.jpg '/>
+return(
 
-        </SwiperSlide>
+
+  <SwiperSlide key={item.link} className="parent-slider">
+  <img src={item.link}/>
+  <Box
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        position: "absolute",
+                        left: "10%",
+                        textAlign: "left",
+                      },
+  
+                      [theme.breakpoints.down("sm")]: {
+                        pt: 4,
+                        pb: 6,
+                      },
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        color: "#222",
+                      }}
+                      variant="h5"
+                    >
+                      LIFESTYLE COLLECTION
+                    </Typography>
+  
+                    <Typography
+                      sx={{
+                        color: "#222",
+                        fontWeight: 500,
+                        my: 1,
+                      }}
+                      variant="h3"
+                    >
+            {item.text}
+                    </Typography>
+  
+                    <Stack
+                      sx={{
+                        justifyContent: { xs: "center", sm: "left" },
+                      }}
+                      direction={"row"}
+                      alignItems={"center"}
+                    >
+                      <Typography color={"#333"} mr={1} variant="h4">
+                        SALE UP TO
+                      </Typography>
+                      <Typography color={"#D23F57"} variant="h4">
+                        30% OFF
+                      </Typography>
+                    </Stack>
+                    <Typography
+                      sx={{
+                        color: "#000",
+                        fontWeight: 300,
+                        my: 1,
+                      }}
+                      variant="body1"
+                    >
+                      Get Free Shipping on orders over $99.00
+                    </Typography>
+  
+                    <Button
+                      sx={{
+                        px: 5,
+                        py: 1,
+                        mt: 2,
+                        backgroundColor: "#222",
+                        boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
+                        color: "#fff",
+                        borderRadius: "1px",
+                        "&:hover": {
+                          bgcolor: "#151515",
+                          boxShadow: "0px 4px 16px rgba(43, 52, 69, 0.1)",
+                        },
+                      }}
+                      variant="contained"
+                    >
+                      shop now
+                    </Button>
+                  </Box>
+          </SwiperSlide>
+     
+
+)
+
+        })}
+     
    
       </Swiper>
 
