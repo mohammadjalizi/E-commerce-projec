50e9a2f57bc56bd9fe78001d0377ea8bd53150f0
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-27 06:58:55 +0330

    Add box and stack our main

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,24 @@
+import { Box, Container, Stack, Typography } from '@mui/material'
 import React from 'react'
 
-export const Main = () => {
+const Main = () => {
   return (
-    <div>Main</div>
+<Container>
+<Stack>
+<Box>
+            <Typography variant="h6">Selected Products</Typography>
+            <Typography fontWeight={300} variant="body1">
+              All our new arrivals in a exclusive brand selection
+            </Typography>
+          </Box>
+
+</Stack>
+<Stack>
+  
+</Stack>
+
+</Container>
   )
 }
+
+export default Main
