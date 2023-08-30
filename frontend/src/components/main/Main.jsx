e3d6ba6e40ab51e4f2465ef812922e2c935e7f7a
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-30 14:58:08 +0330

    Add data

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -4,12 +4,15 @@ import React, { useState } from 'react'
 import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 import CloseIcon from '@mui/icons-material/Close';
 import ProductDetails from './ProductDetails';
+import { useGetproductByNameQuery } from '../../redux/Product';
 
 
 
 
 
 const Main = () => {
+
+
   const theme = useTheme();
   const [alignment, setAlignment] = useState('left');
 
@@ -25,108 +28,111 @@ const Main = () => {
   const handleClose = () => {
     setOpen(false);
   };
+  const { data, error, isLoading } = useGetproductByNameQuery('products?populate=*')
+  console.log(data)
 
-
+ if (data) {
   return (
-<Container  sx={{py:6}} >
-<Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}flexWrap={"wrap"} gap={3}>
-<Box >
-            <Typography variant="h6">Selected Products</Typography>
-            <Typography fontWeight={300} variant="body1">
-              All our new arrivals in a exclusive brand selection
+    <Container  sx={{py:6}} >
+    <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}flexWrap={"wrap"} gap={3}>
+    <Box >
+                <Typography variant="h6">Selected Products</Typography>
+                <Typography fontWeight={300} variant="body1">
+                  All our new arrivals in a exclusive brand selection
+                </Typography>
+              </Box>
+              <ToggleButtonGroup
+               sx={{
+                ".Mui-selected": {
+                  border: "1px solid rgba(233, 69, 96, 0.5) !important",
+                  color: "#e94560",
+                  backgroundColor: "initial",
+                },
+              }}
+              color='error'
+          value={alignment}
+          exclusive
+          onChange={handleAlignment}
+          aria-label="text alignment"
+        >
+          <ToggleButton  sx={{color:theme.palette.text.primary}}  className='myButton'   value="left" aria-label="left aligned">
+             All Products
+          </ToggleButton>
+          <ToggleButton  sx={{color:theme.palette.text.primary,mx: "16px !important"}}   className='myButton'   value="center" aria-label="centered">
+          MEN category
+          </ToggleButton>
+          <ToggleButton  sx={{color:theme.palette.text.primary}}   className='myButton'  value="right" aria-label="right aligned">
+                Women category
+          </ToggleButton>
+         
+        </ToggleButtonGroup>
+    </Stack>
+    
+    {/* carttttt */}
+    <Stack direction={"row"}flexWrap={"wrap"} justifyContent={"space-between"}>
+    
+    {data.data.map((item)=>{
+    return(
+    <Card key={item}  sx={{
+                        maxWidth: 333,
+                        mt: 6,
+                        ":hover .MuiCardMedia-root ": {
+                          rotate: "1deg",
+                          scale: "1.1",
+                          transition: "0.35s",
+                        },
+                      }}>
+          <CardMedia
+            sx={{ height: 277 }}
+            image="https://mui.com//static/images/cards/contemplative-reptile.jpg"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
             </Typography>
-          </Box>
-          <ToggleButtonGroup
-           sx={{
-            ".Mui-selected": {
-              border: "1px solid rgba(233, 69, 96, 0.5) !important",
-              color: "#e94560",
-              backgroundColor: "initial",
-            },
-          }}
-          color='error'
-      value={alignment}
-      exclusive
-      onChange={handleAlignment}
-      aria-label="text alignment"
-    >
-      <ToggleButton  sx={{color:theme.palette.text.primary}}  className='myButton'   value="left" aria-label="left aligned">
-         All Products
-      </ToggleButton>
-      <ToggleButton  sx={{color:theme.palette.text.primary,mx: "16px !important"}}   className='myButton'   value="center" aria-label="centered">
-      MEN category
-      </ToggleButton>
-      <ToggleButton  sx={{color:theme.palette.text.primary}}   className='myButton'  value="right" aria-label="right aligned">
-            Women category
-      </ToggleButton>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions sx={{justifyContent:"space-between"}}>
      
-    </ToggleButtonGroup>
-</Stack>
-
-{/* carttttt */}
-<Stack direction={"row"}flexWrap={"wrap"} justifyContent={"space-between"}>
-
-{["a","c","d"].map((item)=>{
-return(
-<Card key={item}  sx={{
-                    maxWidth: 333,
-                    mt: 6,
-                    ":hover .MuiCardMedia-root ": {
-                      rotate: "1deg",
-                      scale: "1.1",
-                      transition: "0.35s",
-                    },
-                  }}>
-      <CardMedia
-        sx={{ height: 277 }}
-        image="https://mui.com//static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions sx={{justifyContent:"space-between"}}>
- 
-
-      <Button  onClick={handleClickOpen}  size="large"> 
-      <AddShoppingCartOutlinedIcon
-                        sx={{ mr: 1 }}
-                        fontSize="small"
-                      />
-      Add to cart
-      </Button>
-      <Rating precision={0.5} name="read-only" value={4.5} readOnly />
-      </CardActions>
-    </Card>
-
-)
-
-})}
-
-</Stack>
-
-
-<Dialog sx={{".MuiPaper-root":{ minWidth:{xs:"100%", md:800} }}}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-
-<IconButton sx={{position:"absolute",top:0,right:0,":hover":{rotate:"160deg", color:"red", transition:"0.5s"}}}  onClick={handleClose}>
-      <CloseIcon/>
-              
-    </IconButton>
-<ProductDetails />
-      </Dialog>
-</Container>
-  )
+    
+          <Button  onClick={handleClickOpen}  size="large"> 
+          <AddShoppingCartOutlinedIcon
+                            sx={{ mr: 1 }}
+                            fontSize="small"
+                          />
+          Add to cart
+          </Button>
+          <Rating precision={0.5} name="read-only" value={4.5} readOnly />
+          </CardActions>
+        </Card>
+    
+    )
+    
+    })}
+    
+    </Stack>
+    
+    
+    <Dialog sx={{".MuiPaper-root":{ minWidth:{xs:"100%", md:800} }}}
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+    
+    <IconButton sx={{position:"absolute",top:0,right:0,":hover":{rotate:"160deg", color:"red", transition:"0.5s"}}}  onClick={handleClose}>
+          <CloseIcon/>
+                  
+        </IconButton>
+    <ProductDetails />
+          </Dialog>
+    </Container>
+      ) 
+ }
 }
 
 export default Main
--- frontend/src/redux/Product.jsx
+++ frontend/src/redux/Product.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const productApi = createApi({
   reducerPath: 'productApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://http://localhost:1337/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/api/products?populate=*' }),
   endpoints: (builder) => ({
     getproductByName: builder.query({
       query: (name) => `${name}`,
